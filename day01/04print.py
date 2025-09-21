b7973530ddbaa4b02298ff3bf52a1dcd4764513b
wlh <3524835099@qq.com>
2025-09-21 22:35:23 +0800

    1111

---
+++ day01/04print.py
@@ -0,0 +1,15 @@
+#单双引号可替换
+print('he said "good!"')
+#里面含有引号，前放反斜杠表明单纯引号
+print("he said \"let\'s go\"")
+#换行\n
+print("hello!\nhi!")
+#三引号换行
+print('''1
+2
+3
+4''')
+#空格须在内容内
+print('你好' + ' 这是一句程序')
+#空行
+print('\n')
---
+++ day01/05python变量.py
@@ -0,0 +1,7 @@
+#赋值后记得框引号
+my_love='13982443786'
+print('请拨打：' + my_love)
+#覆盖或保留
+my_deepest_love=my_love
+my_love='11111'
+print(my_deepest_love+ my_love)
