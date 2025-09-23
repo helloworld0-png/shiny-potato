1df72fce409dad727204b838a5b256ac7202762e
wlh <3524835099@qq.com>
2025-09-23 12:55:29 +0800

    这是第二次测试提交；

---
+++ day01/07数学运算.py
@@ -0,0 +1,23 @@
+'''
+import math
+math.函数名
+'''
+'''平方是：**2
+乘号不省略 '''
+import math
+print(math.sin(1))
+import math
+result=math.log2(8)
+print(result)
+#求根公式(平方根1/2处需要打括号)
+a=-1
+b=-2
+c=3
+print((-b+(b**2-4*a*c)**(1/2))/2*a)
+print((-b-(b**2-4*a*c)**(1/2))/2*a)
+print((-b+math.sqrt(b**2-4*a*c))/2*a)
+delta=b**2-4*a*c
+print((-b+math.sqrt(delta))/2*a)
+print((-b-math.sqrt(delta))/2*a)
+
+
---
+++ day01/08python数据类型.py
@@ -0,0 +1,26 @@
+'''
+字符串str"你好"
+整数int
+浮点float
+布尔类型bool(True False
+空值类型NoneType
+'''
+#字符串长度
+print(len('你好。\n'))
+a='hello world!'
+print(len(a))
+#提取字符串上单个字(程序中从0开始检索)
+print("hello"[4])
+print(a[7])
+print(a[len(a)-2])
+#布尔类型
+b=True
+c=False
+#空值类型
+n=None
+#Type
+print(type(a))
+print(type(n))
+print(type(b))
+print(type(1.5))
+print(type(2))
